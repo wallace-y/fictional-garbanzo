b5a8f9f0bd399979005dca307c2b746591355d55
wallace-y <cameronwfinney@gmail.com>
2023-06-26 20:01:15 +0100

    adding landing page

---
+++ components/CouponCreator.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text } from "react-native-paper";
+
+const CouponCreator = () => (
+  <>
+    <Text variant="displayMedium">
+      This will be where people can create coupons.
+    </Text>
+  </>
+);
+
+export default CouponCreator;
